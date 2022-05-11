e88b4f70b4e77b74b2b1efab08b4428a383ff628
Shubhamdudhal9903 <105193039+Shubhamdudhal9903@users.noreply.github.com>
2022-05-11 11:01:33 +0530

    Add files via upload

---
+++ Hands-on/arthemtic.java
@@ -0,0 +1,11 @@
+package corejava;
+
+public class arthemtic {
+	public static void main(String[]args) {
+		int a = 100 , b = 50 ,x = a + b , y = a - b , z = a*b , q = a/b;
+		System.out.println("addition::" + x );
+		System.out.println("substraction:" + y);
+		System.out.println("multiply :" + z);
+		System.out.println("division :" + q);
+	}
+}
