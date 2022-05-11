146dd96831c206701a42ae1a70d6e5c771e1aeed
Shubhamdudhal9903 <105193039+Shubhamdudhal9903@users.noreply.github.com>
2022-05-11 12:46:52 +0530

    Add files via upload

---
+++ Hands-on/shubham forloop.java
@@ -0,0 +1,12 @@
+package corejava;
+
+public class ifloop {
+    public static void main (String[]args)
+    {
+    	int i = 1;
+    	for(i = 1; i<=10; ++i)
+    	{
+    		System.out.println( i * 2 );
+    	}
+    }
+}
