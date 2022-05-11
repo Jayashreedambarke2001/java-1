bd7b913732cdfee3bb6d357145b28700a3f112a7
Shubhamdudhal9903 <105193039+Shubhamdudhal9903@users.noreply.github.com>
2022-05-11 19:03:39 +0530

    Add files via upload

---
+++ Hands-on/forloop1.java
@@ -0,0 +1,15 @@
+
+	package corejava;
+
+	public class forloop1 {
+	    public static void main (String[]args)
+	    {
+	    	int i = 1;
+	    	for(i = 1; i<=10; ++i)
+	    	{
+	    		int k = 2 * i;
+	    		System.out.println(i +"*" + 2  +"=" + k);
+	    	}
+	    }
+	
+}
